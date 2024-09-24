22e98e36f96a54d5e324db2e91504d47be20c298
romeoaditya <romeoaditya22@gmail.com>
2024-09-24 15:08:07 +0700

    coba test deploy

---
+++ app/Filament/Resources/CatatanResource/Pages/EditCatatan.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\CatatanResource\Pages;
+
+use App\Filament\Resources\CatatanResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCatatan extends EditRecord
+{
+    protected static string $resource = CatatanResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
